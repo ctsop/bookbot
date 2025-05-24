8ee18ef40c8166f4bcc9513eb492b3db202e3051
ctsop <cmctsop@gmail.com>
2025-05-24 23:12:39 +0900

    added word count function to main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def get_book_text(book):
     with open(book) as f:
         return f.read()
     
+def count_words(result):
+    return len(result.split())
+    
 def main():
     result = get_book_text("books/frankenstein.txt")
-    print(result)
+    num_words = count_words(result)
+    print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
     main()
